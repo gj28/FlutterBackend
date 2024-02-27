88449388f94f060a249473f93443c3c05e1a8501
Gaurav Jadhav <gauravjadhav173@gmail.com>
2024-02-27 22:26:39 +0530

    Email

--- auth/authentication.js
+++ auth/authentication.js
@@ -1,45 +1,101 @@
 const db = require('../db');
 const bcrypt = require('bcrypt');
-//const jwtUtils = require('../token/jwtUtils');
+const jwtUtils = require('../token/jwtUtils');
 const jwt = require("jsonwebtoken");
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
 
 function registerUser(req, res) {
-    const {
-      fullName,
-      contactNo,
-      userType,
-      personalEmail,
-      password
-    } = req.body;
-  
-    const userId = generateUserID();
-    const fetchUserName = `SELECT * FROM app.users WHERE personalemail = $1`;
-    const insertUserQuery = `INSERT INTO app.users(userid, fullname, contactno, usertype, personalemail, password) VALUES($1, $2, $3, $4, $5, $6)`;
-    
-    db.query(fetchUserName, [personalEmail], (fetchUsernameError, fetchUsernameResult) => {
-        //console.log(fetchUsernameError);
-      if (fetchUsernameError) {
-        return res.status(401).json({ message: 'Error Checking User Email' });
+  const {
+    fullName,
+    contactNo,
+    userType,
+    personalEmail,
+    password
+  } = req.body;
+
+  const userId = generateUserID();
+  const fetchUserName = `SELECT * FROM app.users WHERE personalemail = $1`;
+  const insertUserQuery = `INSERT INTO app.users(userid, fullname, contactno, usertype, personalemail, password, verificationtoken, verified) VALUES($1, $2, $3, $4, $5, $6, $7, $8)`;
+
+  db.query(fetchUserName, [personalEmail], (fetchUsernameError, fetchUsernameResult) => {
+    if (fetchUsernameError) {
+      return res.status(401).json({ message: 'Error Checking User Email' });
+    }
+    if (fetchUsernameResult.rows.length > 0) {
+      return res.status(401).json({ message: 'User Already Exists' });
+    }
+    bcrypt.hash(password, 10, (error, hashedPassword) => {
+      if (error) {
+        return res.status(401).json({ message: 'Error During Hashing Password' });
       }
-      if (fetchUsernameResult.rows.length > 0) {
-        return res.status(401).json({ message: 'User Already Exists' });
-      }
-      bcrypt.hash(password, 10, (error, hashedPassword) => {
-        if (error) {
-          return res.status(401).json({ message: 'Error During Hashing Password' });
+      const verificationToken = jwtUtils.generateToken({ personalEmail: personalEmail });
+      db.query(insertUserQuery, [userId, fullName, contactNo, userType, personalEmail, hashedPassword, verificationToken, '0'], (insertUserError, insertUserResult) => {
+        if (insertUserError) {
+          console.error('Error during user insertion:', insertUserError);
+          return res.status(500).json({ message: 'Internal server error' });
         }
-        db.query(insertUserQuery, [userId, fullName, contactNo, userType, personalEmail, hashedPassword], (insertUserError, insertUserResult) => {
-          if (insertUserError) {
-            //console.log(insertUserError);
-            return res.status(401).json({ message: 'Error during Inserting User' });
-          }
-          return res.status(200).json({ message: 'User Added Successfully' });
-        });
+        try {
+          sendTokenEmail(personalEmail, verificationToken);
+          console.log('User registered successfully');
+          return res.status(200).json({ message: 'Registration successful. Check your email for the verification token.' });
+        } catch (sendTokenError) {
+          console.error('Error sending verification token:', sendTokenError);
+          return res.status(500).json({ message: 'Internal server error' });
+        }
       });
     });
-  }
+  });
+}
+
   
-  
+function sendTokenEmail(email, token) {
+
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: "kpohekar19@gmail.com",
+      pass: "woptjevenzhqmrpp"
+    },
+  });
+
+  // Read the email template file
+  const templatePath = path.join(__dirname, '../mail-body/email-template.ejs');
+  fs.readFile(templatePath, 'utf8', (err, templateData) => {
+    if (err) {
+      console.error('Error reading email template:', err);
+      return;
+    }
+
+    // Compile the email template with EJS
+    const compiledTemplate = ejs.compile(templateData);
+
+    // Render the template with the token
+    const html = compiledTemplate({ token });
+
+    const mailOptions = {
+      from: 'kpohekar19@gmail.com',
+      to: email,
+      subject: 'Registration Token',
+      html: html,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+
+    });
+  });
+} 
+
 
 function getUserById(req, res) {
     const userId = req.params.userId;
